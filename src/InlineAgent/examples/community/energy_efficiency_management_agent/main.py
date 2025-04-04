caefeff0b6331e56a5d8639cdf4b15d635970dd8
Eashan Kaushik <50113394+EashanKaushik@users.noreply.github.com>
2025-04-03 20:37:14 -0700

    MCP mac (#93)

--- src/InlineAgent/examples/community/energy_efficiency_management_agent/main.py
+++ src/InlineAgent/examples/community/energy_efficiency_management_agent/main.py
@@ -1,51 +1,25 @@
-#################################################
-# This example is currently under construction #
-###############################################
-
 import asyncio
+import uuid
 
 from InlineAgent.agent import CollaboratorAgent, InlineAgent
 from InlineAgent.knowledge_base import KnowledgeBasePlugin
 from InlineAgent.types import InlineCollaboratorAgentConfig
 from InlineAgent import AgentAppConfig
+from mcp import StdioServerParameters
 
+from InlineAgent.tools import MCPStdio
+from InlineAgent.action_group import ActionGroup
 from tools import forecast_functions, peak_functions
 from prompt import forecast_agent_instruction, peak_agent_instruction
 
-
 config = AgentAppConfig()
 
-forecast_kb = KnowledgeBasePlugin(
-    name=config.FORECAST_KNOWLEDGE_BASE_NAME,
-    description="Access this knowledge base when needing to explain specific forecast generation methodology.",
-    profile="default",
+forecast_agent = CollaboratorAgent(
+    agent_name=config.FORECAST_AGENT_NAME,
+    agent_alias_id=config.FORECAST_AGENT_ALIAS_ID,
+    routing_instruction="""Delegate energy consumption analysis and forecasting tasks to the Forecasting Agent, ensuring adherence to its specific protocols and capabilities.""",
+    relay_conversationHistory="TO_COLLABORATOR",
 )
-
-forecast_agent = InlineAgent(
-    foundation_model="us.anthropic.claude-3-5-sonnet-20240620-v1:0",
-    instruction=forecast_agent_instruction,
-    action_groups=[
-        {
-            "name": "CodeInterpreter",
-            "builtin_tools": {"parentActionGroupSignature": "AMAZON.CodeInterpreter"},
-        },
-        {
-            "name": "ForecastConsumption",
-            "lambda_name": config.FORECAST_LAMNDA_NAME,
-            "function_schema": forecast_functions,
-        },
-    ],
-    knowledge_bases=[forecast_kb],
-    agent_name="forecast_agent",
-    collaborator_configuration={
-        "relayConversationHistory": "TO_COLLABORATOR",
-        "instruction": "Delegate energy consumption analysis and forecasting tasks to the Forecasting Agent, ensuring adherence to its specific protocols and capabilities.",
-    },
-)
-
-# asyncio.run(forecast_agent.invoke(input_text="can you give me my forecasted energy consumption? How does it compare with my past energy usage? My customer id is 1"))
-
-# forecast_agent.invoke(inputText="can you give me my past energy consumption? What is my average spending on summer months? My customer id is 1")
 
 solar_agent = CollaboratorAgent(
     agent_name=config.SOLAR_AGENT_NAME,
@@ -54,44 +28,55 @@ solar_agent = CollaboratorAgent(
     relay_conversationHistory="TO_COLLABORATOR",
 )
 
-peak_agent = InlineAgent(
-    foundation_model="us.anthropic.claude-3-5-sonnet-20240620-v1:0",
-    instruction=peak_agent_instruction,
-    agent_name="peak_agent",
-    action_groups=[
-        {
-            "name": "PeakLoad",
-            "lambda_name": config.PEAK_AGENT_LAMBDA_NAME,
-            "function_schema": peak_functions,
-        }
-    ],
-    collaborator_configuration=InlineCollaboratorAgentConfig(
-        relayConversationHistory="TO_COLLABORATOR",
-        instruction="Direct peak load management and energy optimization tasks to the Peak Load Manager Agent, leveraging its analytical capabilities.",
-    ),
+peak_agent = CollaboratorAgent(
+    agent_name=config.PEAK_AGENT_NAME,
+    agent_alias_id=config.PEAK_AGENT_ALIAS_ID,
+    routing_instruction="""Direct peak load management and energy optimization tasks to the Peak Load Manager Agent, leveraging its analytical capabilities.""",
+    relay_conversationHistory="TO_COLLABORATOR",
 )
 
-# asyncio.run(peak_agent.invoke(input_text="What's causing my peak load? My id is 2."))
+async def main():
+    server_params = StdioServerParameters(
+        command="docker",
+        args=["run", "-i", "--rm", "-e", "PERPLEXITY_API_KEY", "mcp/perplexity-ask"],
+        env={"PERPLEXITY_API_KEY": config.PERPLEXITY_API_KEY},
+    )
 
-supervisor_agent = InlineAgent(
-    foundation_model="us.anthropic.claude-3-5-sonnet-20240620-v1:0",
-    instruction="You are a supervisor agent that is responsible for managing the flow of the conversation.",
-    collaborators=[forecast_agent, solar_agent, peak_agent],
-    agent_name="supervisor_agent",
-    agent_collaboration="SUPERVISOR_ROUTER",
-)
+    preplexity_mcp_client = await MCPStdio.create(server_params=server_params)
+    
+    preplexity_action_group = ActionGroup(
+        name="SearchActionGroup",
+        description="This action group is responsible for searching the internet for information.",
+        mcp_clients=[preplexity_mcp_client],
+    )
+    try:
+        
 
-asyncio.run(
-    supervisor_agent.invoke(
-        input_text="Can you give me my forecasted energy consumption month by month? My id is 1"
-    ),
-)
-# # supervisor_agent.invoke(inputText="how can I check if my Sunpower double-X solar panel eletrical consumption is compliant with energy rules")
-# # supervisor_agent.invoke(inputText="What's causing my peak load? My id is 2.")
-# asyncio.run(supervisor_agent.invoke(
-#     inputText="Can you update my forecast for month 03/2025? I will be travelling and my estimate will be 90. My id is 1. Then tell me how can I check if my Sunpower double-X solar panel eletrical consumption is compliant with energy rules?"
-# ))
+        supervisor = InlineAgent(
+            foundation_model="us.anthropic.claude-3-5-sonnet-20241022-v2:0",
+            instruction="You are a supervisor agent that is responsible for managing the flow of the conversation. You can using preplexity to search the web.",
+            agent_name="supervisor_agent",
+            action_groups=[preplexity_action_group],
+            collaborators=[forecast_agent, solar_agent, peak_agent],
+            agent_collaboration="SUPERVISOR",
+        )
+        
+        session_id = str(uuid.uuid4())
+        
+        while True:
+            user_query = input("Assistant: How can I help you?\n")
 
-#################################################
-# This example is currently under construction #
-###############################################
+            await supervisor.invoke(
+                input_text=user_query,
+                session_id=session_id
+            )
+
+    finally:
+
+        await preplexity_mcp_client.cleanup()
+
+
+if __name__ == "__main__":
+    import asyncio
+
+    asyncio.run(main())
